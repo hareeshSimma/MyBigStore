bcc995cbce77b4fe6baa2b2d51ed4d5c68e3f60d
Tanuja <tanuja1607@gmail.com>
2017-11-30 11:07:43 +0530

    login

--- bigstore-frontend/src/app/login/signup/signup.component.ts
+++ bigstore-frontend/src/app/login/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { User } from '../../shared/models/user.model';
+import { DashboardService } from '../../shared/services/dashboard.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -8,12 +9,21 @@ import { Router } from '@angular/router';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private router:Router) { }
+user:User;
+  constructor(private router:Router,private dashboardService:DashboardService,) { }
   cancel(){
     this.router.navigate(['']);
     
   }
   ngOnInit() {
+    
   }
+  
+  newUser(user){
+    this.dashboardService.newUser(user).subscribe(res=>{
+     console.log(user);
+    })
+
+    }
 
 }
